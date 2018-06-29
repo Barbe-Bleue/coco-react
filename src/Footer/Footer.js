1e0ef33edeaef685ec0def49e7014dc91d0fc1b0
Barbe-Bleue <screy@icloud.com>
2018-06-29 23:28:15 +0200

    update footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,13 +3,18 @@ import React from 'react';
 import {Header} from 'react-native-elements'
 import {
   View,
-  StyleSheet, Text
+  StyleSheet,
+  Text,
+  TouchableHighlight
 } from 'react-native';
 export default class Footer extends React.Component {
   render() {
     return (
-      <View>
-        <Text>SALUT</Text>
+      <View style={{height:100}}>
+        <TouchableHighlight onPress={() => Linking.openURL('http://github.com/Barbe-Bleue')}>
+          <Text>App created by Barbe-Bleue: </Text>
+        </TouchableHighlight>
+
       </View>
     );
   }

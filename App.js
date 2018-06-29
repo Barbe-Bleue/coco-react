be14647c751b8a24b926eb87eb3a3f929d241aa6
Barbe-Bleue <screy@icloud.com>
2018-06-29 20:16:17 +0200

    add Footer Component

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Commit from './src/Commit/Commit'
 import Head from './src/Header/Header'
+import Footer from './src/Footer/Footer'
 import {
   View,
   ScrollView,
   StyleSheet, Text
 } from 'react-native';
+
 export default class App extends React.Component {
+
   render() {
     return (
+
+      <View>
         <Head/>
+        <ScrollView style={style.container}>
+          <Commit/>
+        </ScrollView>
+        <Footer/>
       </View>
     );
   }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Header} from 'react-native-elements'
+import {
+  View,
+  StyleSheet, Text
+} from 'react-native';
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>SALUT</Text>
+      </View>
+    );
+  }
+}

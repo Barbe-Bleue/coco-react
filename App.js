3671d6370c58ea6b1a46a12638c8060ba9b3b25c
Barbe-Bleue <screy@icloud.com>
2018-06-29 20:16:08 +0200

    add header component

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Commit from './src/Commit/Commit'
+import Head from './src/Header/Header'
 import {
   View,
-  StyleSheet
+  ScrollView,
+  StyleSheet, Text
 } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={style.container}>
-        <Commit/>
+        <Head/>
       </View>
     );
   }
---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header} from 'react-native-elements'
+
+export default class Head extends React.Component {
+  render() {
+return(
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+          outerContainerStyles={{ backgroundColor: '#000' }}
+
+        />
+    );
+  }
+}
